1f4cca435fb568c61a8c56b8d4624be10c84e41b
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2015-11-08 17:28:08 -0700

    Switched to  to avoid responders mess.
    The api wasn't correctly exposing the json while using
    . I determined we're only rendering json for this project and I don't
    wish to sink anymore time into the backend of the api.

--- app/controllers/api/v1/properties_controller.rb
+++ app/controllers/api/v1/properties_controller.rb
@@ -1,13 +1,13 @@
 class Api::V1::PropertiesController < Api::V1::BaseController
   def index
-    respond_with Property.all
+    render json: Property.all
   end
 
   def show
-    respond_with Property.find_by(id: params[:id])
+    render json: Property.find_by(id: params[:id])
   end
 
   def runewords
-    respond_with Property.find_by(id: params[:property_id]).runewords
+    render json: Property.find_by(id: params[:property_id]).runewords
   end
 end
