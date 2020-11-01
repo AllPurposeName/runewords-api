929a4eff52a3f42aec37fe82fdfca58b5cb77e7a
DJ Greenfield <mr.kaitryn.dale@gmail.com>
2020-10-31 19:22:25 -0600

    Fix properties controller tests

--- spec/controllers/api/v1/properties_controller_spec.rb
+++ spec/controllers/api/v1/properties_controller_spec.rb
@@ -17,8 +17,9 @@ RSpec.describe Api::V1::PropertiesController, type: :controller do
     context '#index' do
       get "#{root_path}/properties" do
         example_request 'returns all properties' do
+          last_response_object = JSON.parse(response_body).find { |b| b['name'] == JSON.parse(schema)['name'] }.to_json
           expect(status).to eq(200)
-          expect(response_body).to match_json([schema])
+          expect(last_response_object).to match_json(schema)
         end
       end
     end
@@ -34,10 +35,11 @@ RSpec.describe Api::V1::PropertiesController, type: :controller do
     end
 
     context '#runewords' do
+      let(:id) { Property.first.id }
       get "#{root_path}/properties/:id/runewords" do
         example_request 'returns all associated runewords' do
           expect(status).to eq(200)
-          expect(response_body).to eq(Property.first.runewords.to_json)
+          expect(response_body).to include(Property.first.runewords.first.name)
         end
       end
     end
